289a2c499acdd6bd4e7e6565cd3a766cf2c91571
Mike Hallworth <mhallworth@gmail.com>
2017-10-17 23:09:01 -0400

    Added plotting functions

--- Migration_SOSA589.R
+++ Migration_SOSA589.R
@@ -82,19 +82,24 @@ cap.long <- -149.75
 
 # Set the seed
 # The time when we know the geolocator recorded no light.
-seed <- as.POSIXct(c(rep("2016-12-01 04:00:00", 3), rep("2017-01-01 04:00:00",
-8), rep("2017-02-01 04:00:00", 4)), format = "%Y-%m-%d %H:%M:%S", tz = "GMT")
+# You don't need that many replicates of the dates - I removed the ones that weren't needed
+seed <- as.POSIXct(c("2016-12-01 04:00:00", 
+                     "2017-01-01 04:00:00", 
+                     "2017-02-01 04:00:00"), format = "%Y-%m-%d %H:%M:%S", tz = "GMT")
 
 twl  <- findTwilights(tagdata = d.lux, 
                       threshold = 1.5, 
                       include = seed,
                       dark.min = 0) # minimum dark period in minutes
 
-# Create a vector of the dates the geolocator was recording data
-tm <- seq(d.lux[1,1], d.lux[nrow(d.lux),1], by = "day")
-head (tm)
-# Create a vector of TRUE /FALSE to determine whether it's sunrise or sunset
-rise <- rep(c(TRUE, FALSE), length(tm))
+##### Plot the light data and the twilights ######
+lightImage(tagdata = d.lux,
+           offset = 18)
+tsimagePoints(twl$Twilight, 
+              offset = 19, 
+              pch = 16, cex = 0.5, 
+              col = ifelse(twl$Rise, "blue", "red"))
+
 
 #Edit the transitions
 #Moves or deletes outliers
@@ -114,8 +119,54 @@ twl <- subset(twl, Deleted == FALSE)
 
 # Zenith Angle--------------------------------------------------------------------------------------------------
 
-# Create a vector with the dates known to be at deployment 
-calib.dates <- as.POSIXct(c("2016-06-13","2017-06-01"))
+# Create a vector with the dates known to be at deployment
+
+# These dates should be changed a bit - as of right now the whole year is used as calibration dates
+# what you want are dates when you know that the bird is at the deployment site. This can be a bit 
+# tricky for near arctic breeding birds when tags are deployed during the summer. I moved some things
+# around. Here, I use the lightImage and the cal.data to determine when the bird arrives in the spring
+# I use those data until incubation starts to calculate zenith angles. Just after deployment doesn't 
+# work that well in this case because of the nearly constant light issues. 
+
+# Create a vector of the dates the geolocator was recording data
+tm <- seq(d.lux[1,1], d.lux[nrow(d.lux),1], by = "day")
+head (tm)
+
+# Create a vector of TRUE /FALSE to determine whether it's sunrise or sunset
+rise <- rep(c(TRUE, FALSE), length(tm))
+
+# making predicted twilight times given location and zenith #
+cal.data<- data.frame(Twilight = twilight(rep(tm, each = 2),
+                           lon = CapLocs[1], 
+                           lat = CapLocs[2], 
+                           rise = rise, zenith = 95),
+                           Rise = rise) 
+
+##### Plot the light data and sunrise suset times at capture location ######
+lightImage(tagdata = d.lux,
+           offset = 18,
+           zlim = c(0,5))
+tsimagePoints(cal.data$Twilight, 
+              offset = 19, 
+              pch = 16, cex = 0.5, 
+              col = ifelse(twl$Rise, "red", "blue"))
+tsimagePoints(twl$Twilight, 
+              offset = 19, 
+              pch = 16, cex = 0.5, 
+              col = ifelse(twl$Rise, "blue", "red"))
+
+#Using this next bit of code you can click on the lightImage plot and find dates 
+# as.POSIXct(locator(n = 2)$x,origin = "1970-01-01",tz = "GMT")
+# "2017-05-08 04:15:19 GMT" "2017-05-17 17:40:58 GMT"
+
+# This plots a polygon around the area that I used to determine Zenith Angles #
+polygon(x = as.POSIXct(c("2017-05-08","2017-05-17","2017-05-17","2017-05-08")),
+        y = c(4+24,4+24,14+24,14+24), 
+        col = rgb(100,100,100,100,maxColorValue = 255),
+        border = rgb(100,100,100,100,maxColorValue = 255))
+
+
+calib.dates <- as.POSIXct(c("2017-05-08","2017-05-17"))
 
 # Extract twilight data during calibration period
 cal.data<-subset(twl,twl$Twilight>=calib.dates[1] & twl$Twilight<=calib.dates[2])
@@ -152,7 +203,7 @@ hist(twl_dev, freq = F,
      yaxt="n",
      ylim = c(0, 0.15),
      xlim = c(0, 60),
-     breaks=15,
+     breaks=3,
      main = "",
      xlab = "Twilight error (mins)")
 axis(2,las=2)
@@ -171,10 +222,16 @@ points(1,max(z,na.rm=TRUE),col="red",pch=20)
 
 # Initial Locations----------------------------------------------------------------------------------------------
 
-# How do I set tolernace values around equinox for South America? 
+# How do I set tolernace values around equinox for South America?  
+   # see code below
 # How do I remove erroneous migration paths such as those along the east coast of the United States during equinox?
+   # see tol code below
 # How do I remove the migration paths into the Gulf of Alaska? These may be due to light-level fluctuations during
 # incubation in June 2017?
+   # There are several ways to deal with this - 1) set fixed = TRUE for the month of May - June. 2) Not include those
+   # dates because it's already back on the breeding grounds  where you caught it (presumably) and locations are 
+   # compromised because of incubation. Here - I set the fixed = TRUE for those dates where the incubation seems to
+   # compromise the location data. 
 
 
 # Obtain the initial path based on a simple threshold analysis
@@ -183,8 +240,17 @@ d.twl <-subset(twl,twl$Twilight>=calib.dates[1] & !Deleted)
 path <- thresholdPath(twilight = d.twl$Twilight, 
                       rise = d.twl$Rise, 
                       zenith=zenith0,
-                      tol= c(0.09,0.1)) # Here is where you can mess with the tol values
-                                        # the first value is fall - the second is spring
+                      tol= c(0.16,0.09)) # Here is where you can mess with the tol values
+                                          # the first value is fall - the second is spring
+
+# NOTE - setting the tol values is a bit trial and error - I settled one these two values because they seem 
+# to fit the general trajectory of the migration. For example, the spring and fall tracks over the Andes appears
+# to be roughly in the same place. Larger tol values in the fall have the bird going up through Florida (didn't)
+# seem right given a few points were near/on the Yucatan. Also, one of the potential stop-overs appears to be 
+# similar in the fall and spring on Ecuador/Peru border. Larger tol values smooth that out. Ultimately, these
+# values need to be justified i.e., maximized the amount of time on land, matched similar route in the spring etc.
+# However, in the end these values (lat,long) are the starting values for the MCMC chain but they could potentially
+# be informative. 
 
 layout(matrix(c(1,1,3,3,3,
                 2,2,3,3,3),2,5,byrow = TRUE))
@@ -193,7 +259,7 @@ par(mar = c(2,4,1,1)+0.1)
 plot(path$time, path$x[, 2], type = "b", pch = 16, cex = 0.5, ylab = "Lat", xlab = '')
 abline(h = cap.lat)
 abline(v = as.POSIXct("2016-09-23"),col="red",lty=2,lwd=1.5)
-abline(v = as.POSIXct("2017-03-20"),col="red",lty=2,lwd=1.5)9
+abline(v = as.POSIXct("2017-03-20"),col="red",lty=2,lwd=1.5)
 
 plot(path$time, path$x[,1],type="b",pch=16,cex=0.5,ylab="Long",xlab="")
 abline(h = cap.long)
@@ -204,6 +270,7 @@ plot(path$x, type = "n",ylim=c(-60,60),xlim=c(-180,-55))
 plot(wrld_simpl, add = T, col = "grey95")
 lines(path$x, col = "blue")
 points(path$x, pch = 16, cex = 0.5, col = "blue")
+points(CapLocs[1],CapLocs[2],pch = 19, col = "red", cex = 2)
 
 #Take the location estimates created above
 x0<- path$x
@@ -214,18 +281,26 @@ z0<- trackMidpts(x0)
 # Set flight parameters
 beta <- c(0.7, 0.08)
 
+
+
 # This function sets the known location of the bird - capture location and recapture location
 # This is where you can add ancillary resighting events; known locations throughout annual cycle
+# Here I added Fixed = TRUE to the end of the file  - from May 08 on.
+
+# Find which row in the twl file is May 08 2017 # 
+May08 <- max(which(strptime(twl$Twilight,format = "%Y-%m-%d") == "2017-05-08"))
+
 fixedx<- rep(F, nrow(x0))
-fixedx[1:5] <- T
-fixedx[(nrow(x0)-5):nrow(x0)] <- T
+fixedx[1:5] <- TRUE
+# Fixed from May - end of file #
+fixedx[May08:nrow(x0)] <- TRUE
 
 x0[fixedx, 1] <- cap.long
 x0[fixedx, 2] <- cap.lat
 
 # Set what xlim and ylim values you need to span the range of your dataset
-xlim <- c(-115, -60)
-ylim <- c(0, 90)
+xlim <- c(-180, -30)
+ylim <- c(-80, 90)
 
 # Spatial Mask (do not use for shorebirds)
 
@@ -263,7 +338,7 @@ proposal.z <- mvnorm(S=diag(c(0.0025,0.0025)),n=nlocation(z0))
 fit <- estelleMetropolis(model = model,
                          proposal.x = proposal.x,
                          proposal.z = proposal.z,
-                         iters=200, # This value sets the number of iterations to run
+                         iters=2000, # This value sets the number of iterations to run
                          thin=5,
                          chains=3)
 
@@ -278,7 +353,7 @@ fit <- estelleMetropolis(model = model,
                               proposal.z = proposal.z,
                               x0 = x0,
                               z0 = z0,
-                              iters=200, # This value sets the number of iterations to run
+                              iters=2000, # This value sets the number of iterations to run
                               thin=5,
                               chains=3)
 
@@ -295,18 +370,151 @@ fit <- estelleMetropolis(model = model,
                          proposal.z = proposal.z,
                          x0 = x0,
                          z0 = z0,
-                         iters=500, # This value sets the number of iterations to run
+                         iters=5000, # This value sets the number of iterations to run
                          thin=10,
                          chains=3)
 
 #Plot--------------------------------------------------------------------------------------------------------------
 
 #This step makes an empty raster #
-r <- raster(xmn = xlim[1], xmx = xlim[2], ymn = ylim[1], ymx = ylim[2], res = c(0.25,
-                                                                                0.25))
+r <- raster(xmn = xlim[1],
+            xmx = xlim[2], 
+            ymn = ylim[1], 
+            ymx = ylim[2], res = c(0.25,0.25))
+
 # set plot parameters 
 xlim = c(-180, 0)
 ylim = c(-60, 80)
+
+### Here is another way to determine the locations and where the bird was ###
+
+S <- SGAT::slices(type="intermediate",
+                 breaks="day",
+                 weights = rep(0.5,length(fit$model$time)),
+                 mcmc=fit,
+                 grid=r)
+
+## Quick lightImage plot to estimate stationary dates ## 
+lightImage(tagdata = d.lux,
+           offset = 18,
+           zlim = c(0,5))
+tsimagePoints(cal.data$Twilight, 
+              offset = 19, 
+              pch = 16, cex = 0.5, 
+              col = ifelse(twl$Rise, "red", "blue"))
+tsimagePoints(twl$Twilight, 
+              offset = 19, 
+              pch = 16, cex = 0.5, 
+              col = ifelse(twl$Rise, "blue", "red"))
+
+# These stationary dates were estimated from the lightImage #
+# you can add as many of these as you want - each stationary time
+# should be a row 
+
+stationary.periods <- data.frame(start = "2016-11-01",
+                                 stop = "2017-03-01")
+
+SOSA <- LLmig::MigSchedule(MCMC = S, 
+                           mig.quantile = 0.95,
+                           stationary.periods = stationary.periods,
+                           stationary.duration = 1,
+                           prob = 0.95,
+                           rm.lat.equinox = FALSE,
+                           days.omit = 0)
+
+# See what the MigSchedule function produces - 
+str(SOSA,1)
+
+#Wish List-------------------------------------------------------------------------------------------------
+#Plot maps for a single southbound and single north bound migration route.
+#Determine and then Plot important stop-over locations.
+
+# Here is how to go about doing that using the results from the MigSchedule function 
+# Take a look at the migration schedule
+SOSA$Schedule
+
+FallMig <- raster::spLines(sp::SpatialPoints(cbind(c(CapLocs[1],SOSA$Schedule[1:7,3]),# Longitudes
+                                                   c(CapLocs[2],SOSA$Schedule[1:7,6]))))
+
+
+SpringMig <- raster::spLines(sp::SpatialPoints(SOSA$Schedule[8:15,c(3,6)]))
+
+plot(FallMig)
+for(i in 1:7){
+plot(SOSA$movements[[i]],add = TRUE,legend = FALSE)
+}
+plot(wrld_simpl, add = TRUE)
+points(CapLocs[1],CapLocs[2],cex = 2,pch = 19)
+
+plot(SpringMig, add = TRUE, col = "red")
+for(i in 8:15){
+plot(SOSA$movements[[i]],
+     col = rev(bpy.colors(25)),
+     add = TRUE,
+     legend = FALSE)
+}
+
+#Plot maps on satellite imagery with state and country borders.
+# There are quite a few ways to do this - but here is one way. 
+# Obtain a BlueMarble image manually then plot the results onto that 
+# The link below is a recent one - not much now cover
+# https://neo.sci.gsfc.nasa.gov/view.php?datasetId=BlueMarbleNG-TB&date=2004-07-01 #
+
+BlueMarb <- raster::brick(list.files("Spatial_Layers/",pattern = "*.TIFF",full.names = TRUE))
+
+# Plot fall migration to set plot region #
+plot(FallMig)
+
+# add map using real-world colors #
+plotRGB(BlueMarb, colNA = "black", maxpixels = 6480000, add = TRUE)
+
+# add the migration path back #
+plot(FallMig, add = TRUE, col = "red")
+
+# add country borders # 
+plot(wrld_simpl, add = TRUE, border = "yellow")
+
+# add one of the stop-over locations #
+plot(SOSA$movements[[5]],add = TRUE, col = bpy.colors(20), alpha = 0.4, legend = FALSE)
+
+##### Here is another way to get border shapefiles ###
+# You can get the data directly from the raster package #
+
+# raster::getData("countries") # this returns an error but the data are available here 
+                               #'http://biogeo.ucdavis.edu/data/gadm2.8/countries_gadm28.rds'
+
+#Canada <- raster::getData("GADM", country = "Canada", level = 1)
+#States <- raster::getData("GADM", country = "United States", level = 1)
+
+#plot(Canada)
+#plot(States, add = TRUE)
+
+# Here is a third way # 
+
+plot(dismo::gmap(FallMig, type = "satellite", lonlat = TRUE))
+plot(FallMig, add = TRUE, col = "red")
+
+#Set tolerance values for South American equinox to remove erroneous migration paths.
+# See above
+
+#Questions-------------------------------------------------------------------------------------------------------
+#What is the ideal number of iterations to have for the MCMC model?
+# The way you have it set up now - 2000 iterations - then summarize, then 2000 then summarize then 5000 
+# is how I've done it in the past - you can do more but as long as you summarize between runs you 
+# are using all the data. A few thousand is probably enough - it looks good for this bird. 
+
+#During incubation, light-levels get skewed. Can I remove data points during the incubation period?
+# I set those locations to fixed  - another option is to just remove them. Both methods work and 
+# acheive pretty much the same thing. 
+
+
+
+
+
+
+
+
+
 opar <- par(mar=c(2,2,2,2)+0.1)
 
 # summary location from the model - 
@@ -353,12 +561,4 @@ for (k in sliceIndices(s)) {   #loop through the slices to plot the data
 }
 par(opar)
 
-#Wish List-------------------------------------------------------------------------------------------------
-#Plot maps for a single southbound and single north bound migration route.
-#Plot maps on satellite imagery with state and country borders.
-#Determine and then Plot important stop-over locations.
-#Set tolerance values for South American equinox to remove erroneous migration paths.
 
-#Questions-------------------------------------------------------------------------------------------------------
-#What is the ideal number of iterations to have for the MCMC model?
-#During incubation, light-levels get skewed. Can I remove data points during the incubation period?
